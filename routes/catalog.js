7fad87cac5f9563f91f66e2e3b0e53b02dabcaaf
Leonardo Severini <leoseverini@gmail.com>
2023-04-11 22:35:51 +0300

    Added short description to 100 chars

--- routes/catalog.js
+++ routes/catalog.js
@@ -21,7 +21,7 @@ router.get('/list', async (req, res, next) => {
             return {
                 id: item.id,
                 name: item.name,
-                short: item.description.substring(0, 60),
+                short: item.description.substring(0, 100),
                 image: item.images[0],
             }
         });
